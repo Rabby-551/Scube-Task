d4f61ec0aaf292ce8fb8bbd5f9c18be48a4f09e3
Rabby-551 <rabby.raziul@gmail.com>
2025-12-16 23:33:59 +0600

    create appcolor and add all color of the design

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  const AppColors._();
+
+  static const Color primary = Color(0xFF0094FF);
+  static const Color textDark = Color(0xFF0F2230);
+  static const Color textBody = Color(0xFF6A6A6A);
+  static const Color border = Color(0xFFC7D3DF);
+  static const Color background = Color(0xFFF8FAFD);
+  static const Color white = Colors.white;
+}
